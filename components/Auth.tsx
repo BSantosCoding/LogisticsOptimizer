da051b12694ed789ed87b751c9fb96ba67ce4a6e
BSantos10 <bernardo.santos@massivestudios.io>
2025-12-11 09:51:27 +0100

    fix auth confirm email

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -34,6 +34,9 @@ const Auth: React.FC = () => {
         const { data: authData, error: authError } = await supabase.auth.signUp({
           email,
           password,
+          options: {
+            emailRedirectTo: window.location.origin,
+          },
         });
 
         if (authError) throw authError;
